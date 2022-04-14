b50bc8a449855067e865d90152a0741a51639c5a
Tarcisio-UFRB <tarcisiosapucaia27@gmail.com>
2022-04-14 11:07:52 -0300

    mod-14-04

--- COD_EDP/ajusteCamara/ajusteCamara.ino
+++ COD_EDP/ajusteCamara/ajusteCamara.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 
 int valor;
-int ad2;
+int ad0;
 
 void setup() {
   Serial.begin(115200); //velocidade da cominicacao com a porta serial
@@ -15,8 +15,18 @@ void loop() {
   if (Serial.available()>1){  
     valor = Serial.parseInt(); //valor em contagem enviado ao DAC [0-4095]
     analogWrite(DAC0, valor);
+    analogWrite(DAC1, 0);
+    delay(5000);
+    int acc = 0;
+    for(int i=0; i<10; i++){
+      ad0 = analogRead(A0); // ler o sensor de pressão da valvula proporcional;
+      acc = acc + ad0;
+      delay(500);
+    }
+    Serial.println(acc/10);
+    Serial.println("----");
   }
-  ad2 = analogRead(A2); // ler o sensor de pressão da valvula proporcional;
-  Serial.println(ad2);
-  delay(500);
+  //ad0 = analogRead(A0); // ler o sensor de pressão da valvula proporcional;
+  //Serial.println(ad0);
+  //delay(500);
 }
