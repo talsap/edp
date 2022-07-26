333a23ed13e38cbc635fc2cbff2ee105ee6707f1
Tarcisio Sapucaia <tarcisiosapucaia27@gmail.com>
2022-07-26 11:43:43 -0300

    mod-26-07

--- setup.py
+++ setup.py
@@ -1,18 +1,39 @@
-import PyInstaller.__main__
+from cx_Freeze import setup, Executable
+import sys
 
-PyInstaller.__main__.run([
-    '--onedir',
-    '--nowindow',
-    '--name=edp',
-    '--noconsole',
-    '--version-file=file_version_info.txt',
-    '--icon=logo.ico',
-    '--add-data=back;back',
-    '--add-data=COD_EDP;COD_EDP',
-    '--add-data=front;front',
-    '--add-data=icons;icons',
-    '--add-data=logo;logo',
-    '--add-data=bancodedados.py;.',
-    '--add-data=bancodedadosCAB.py;.',
-    'edp.py'
-])
+base = None
+
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+executables = [Executable('edp.py', base=base, icon='icons/logo.ico')]
+
+files = ['COD_EDP/','icons/','logo/', 'bancodedados.py', 'bancodedadosCAB.py', 'file_version_indo.txt']
+excludes = ['tkinter', 'xml', 'html', 'future', 'email', 'json', 'http']
+packs = ['wx',
+         're',
+         'sys',
+         'time',
+         'sqlite3',
+         'datetime',
+         'threading',
+         'unicodecsv',
+         'csv',
+         'pubsub',
+         'pandas',
+         'numpy',
+         'matplotlib',
+         'drawnow',
+         'serial',
+         'shutil',
+         'reportlab',
+         'PyPDF2',
+         'math']
+
+setup(
+    name = 'edp',
+    version = '1.0.0',
+    description = 'Ensaios Dinamicos para Pavimentacao',
+    options = {'build_exe':{'include_files':files, "excludes":excludes, 'packages':packs}},
+    executables = executables
+)
