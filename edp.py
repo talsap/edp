7f7228ff408387dd9203335b0e470183c88c0902
Tarcisio de Souza Sapucaia <tarcisiosapucaia27@gmail.com>
2022-08-16 10:35:57 -0300

    mod-16-08

--- edp.py
+++ edp.py
@@ -7,7 +7,7 @@ import front.tela as tela
 import ctypes
 
 '''usado para ocultar o console'''
-#ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 0 )
+ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 0 )
 
 '''Inicializacao do programa'''
 class main():
